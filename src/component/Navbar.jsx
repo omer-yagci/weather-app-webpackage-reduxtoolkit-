6a5ede53339ca5a4eb74f64a044ebe923b0034b4
khan <omeryagci3295@gmail.com>
2022-11-05 17:21:31 +0300

    navbar end

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [show, setShow] = useState(true);
   return (
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-lime-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <div className="flex items-center  text-white mr-6">
           <svg
             className="fill-current h-8 w-8 mr-2"
             width={54}
@@ -16,12 +18,20 @@ const Navbar = () => {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Omer Yagci
-          </span>
+          <a
+            href="https://www.linkedin.com/in/omer-kagan-yagci/"
+            className="design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <code>{"oKaganYAGCI/> "}</code>
+          </a>
         </div>
         <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <button
+            onClick={() => setShow(!show)}
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          >
             <svg
               className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
@@ -35,65 +45,38 @@ const Navbar = () => {
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
             <NavLink
-              className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4"
+              className="block mt-4 lg:inline-block text-lg lg:mt-0 text-black-200 hover:text-white mr-4"
               to="/"
             >
               Home
             </NavLink>
             <NavLink
-              className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4"
+              className="block mt-4 lg:inline-block lg:mt-0  text-base text-black-200 hover:text-white mr-4"
               to="turkeymaps"
             >
               Turkey Map
             </NavLink>
             <NavLink
-              className="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4"
+              className="block mt-4 lg:inline-block lg:mt-0 text-base text-black-200 hover:text-white mr-4"
               to="turkeyprovinces"
             >
               Turkey Provinces
             </NavLink>
           </div>
-          <div>
-            <a
-              target="_blank"
-              href="https://github.com/omer-yagci"
-              rel="noreferrer"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            >
-              Github
-            </a>
-          </div>
-        </div>
-      </nav>
-
-      {/* <nav className="bg-lime-400">
-        <NavLink to="/home">
-          <h2>Omer Yagci</h2>
-        </NavLink>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/">Turkey Map</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Turkey Province</NavLink>
-            </li>
-            <li>
+          {show && (
+            <div>
               <a
                 target="_blank"
-                rel="noreferrer"
                 href="https://github.com/omer-yagci"
+                rel="noreferrer"
+                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
               >
                 Github
               </a>
-            </li>
-          </ul>
+            </div>
+          )}
         </div>
-      </nav> */}
+      </nav>
     </div>
   );
 };
