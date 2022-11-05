ce08dc1e0e2c920207889c00fe7401fac7efb22a
khan <omeryagci3295@gmail.com>
2022-11-05 15:10:47 +0300

    router started

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return <div>AppRouter</div>;
+};
+
+export default AppRouter;
