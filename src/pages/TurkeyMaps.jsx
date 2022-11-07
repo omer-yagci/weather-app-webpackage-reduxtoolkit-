0d179ca6adbc6d7770c6010556e173b4d3027a53
khan <omeryagci3295@gmail.com>
2022-11-07 17:28:43 +0300

    dispacth added

--- src/pages/TurkeyMaps.jsx
+++ src/pages/TurkeyMaps.jsx
@@ -1,7 +1,7 @@
-import Tooltip from "antd/es/tooltip";
+// import Tooltip from "antd/es/tooltip";
 import { useState } from "react";
 import TurkeyMap from "turkey-map-react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getWeather } from "../features/WeatherSlice";
 import { useNavigate } from "react-router-dom";
 
@@ -11,25 +11,26 @@ const TurkeyMaps = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { weatherList } = useSelector((state) => state.weather);
+  // const { weatherList } = useSelector((state) => state.weather);
 
   const ClickHandler = () => {
     dispatch(getWeather(cityName));
     navigate("/");
   };
 
-  const renderCity = (cityComponent, cityData) => (
-    <Tooltip title={cityData.name} key={cityData.id}>
-      {cityComponent}
-    </Tooltip>
-  );
+  // const renderCity = (cityComponent, cityData) => (
+  //   <Tooltip title={cityData.name} key={cityData.id}>
+  //     {cityComponent}
+  //   </Tooltip>
+  // );
+
   return (
     <>
-      <h1>{cityName}</h1>
+      <h5>{cityName}</h5>
       <div>
         <TurkeyMap
+          // cityWrapper={renderCity}
           onClick={ClickHandler}
-          cityWrapper={renderCity}
           onHover={({ name }) => setCityName(`${name}`)}
         />
       </div>
