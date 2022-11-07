574bd830821c5e77c4654b87147d660ebd0a6d60
khan <omeryagci3295@gmail.com>
2022-11-07 14:55:55 +0300

    v 0.1.2.3

--- src/pages/TurkeyMaps.jsx
+++ src/pages/TurkeyMaps.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import Tooltip from "antd/es/tooltip";
+import { useState } from "react";
+import TurkeyMap from "turkey-map-react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWeather } from "../features/WeatherSlice";
 
 const TurkeyMaps = () => {
-  return <div>TurkeyMap</div>;
+  const [cityName, setCityName] = useState("");
+  const dispatch = useDispatch();
+  const { weatherList } = useSelector((state) => state.weather);
+  console.log(weatherList);
+
+  const ClickHandler = (city) => {
+    dispatch(getWeather(cityName));
+  };
+
+  // const renderCity = (cityComponent, cityData) => (
+  //   <Tooltip title={cityData.name} key={cityData.id}>
+  //     {cityComponent}
+  //   </Tooltip>
+  // );
+  console.log(cityName);
+  return (
+    <>
+      <h1>{cityName}</h1>
+      <div>
+        <TurkeyMap
+          onClick={ClickHandler}
+          // cityWrapper={renderCity}
+          onHover={({ plateNumber, name }) => setCityName(`${name}`)}
+        />
+      </div>
+    </>
+  );
 };
 
 export default TurkeyMaps;
