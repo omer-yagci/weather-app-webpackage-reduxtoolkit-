4b76e495807600f4e7212ee3e8a6932e1aa59c6e
khan <omeryagci3295@gmail.com>
2022-11-09 19:50:28 +0300

    province page started

--- src/pages/TurkeyProvinces.jsx
+++ src/pages/TurkeyProvinces.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import useTurkeyCities from "use-turkey-cities";
 
 const TurkeyProvinces = () => {
-  return <div>TurkeyProvince</div>;
+  const { cities, city, setCity, districts } = useTurkeyCities();
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(city, districts);
+        }}
+      >
+        <select
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+          value={city}
+        >
+          {cities.map((city) => (
+            <option key={`city-${city}`} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+        <br />
+
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default TurkeyProvinces;
